4666dea12d5ef351145afad36946b67357fa4e9b
prishitakadam <prishitakadam12@gmail.com>
2021-07-04 13:42:16 +0530

    spin lock inside if

--- rdma.h
+++ rdma.h
@@ -201,9 +201,10 @@ class In_Use_Array{
 
 SpinMutex spinlock;
 int allocate_memory_slot(){
-    spinlock.lock();
     if(!in_use_->empty()){
       //Spin Lock
+      
+      spinlock.lock();
       int index = in_use_->front();
       in_use_->pop();
       spinlock.unlock();

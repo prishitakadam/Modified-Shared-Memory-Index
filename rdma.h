f7051b3defa1eb541e697b57f82280d1920d9d07
prishitakadam <prishitakadam12@gmail.com>
2021-07-04 15:13:05 +0530

    Revert "Both Lock Implemented"
    This reverts commit 4942bbe75fe290718ffefad8d02b02d16dbeb00b.

--- rdma.h
+++ rdma.h
@@ -198,7 +198,12 @@ class In_Use_Array{
 //     }
 //     return -1; //Not find the empty memory chunk.
 //   }
+<<<<<<< HEAD
 // SpinMutex spinlock;
+=======
+
+SpinMutex spinlock;
+>>>>>>> parent of 4942bbe... Both Lock Implemented
 int allocate_memory_slot(){
     if(!in_use_->empty()){
       //Spin Lock

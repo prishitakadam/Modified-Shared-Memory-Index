0b8297e3b66444363adb70349259663b8f45bed2
prishitakadam <prishitakadam12@gmail.com>
2021-07-19 22:12:02 +0530

    std::queue

--- rdma.h
+++ rdma.h
@@ -154,7 +154,8 @@ class In_Use_Array{
   In_Use_Array(size_t size, size_t chunk_size, ibv_mr* mr_ori)
       : element_size_(size), chunk_size_(chunk_size), mr_ori_(mr_ori){
     // in_use_ = new std::atomic<bool>[element_size_];
-    in_use_ = new std::queue<int>;
+    // in_use_ = new std::queue<int>;
+    std::queue<int> in_use_;
     //OLD
     // for (size_t i = 0; i < element_size_; ++i){
     //   in_use_[i] = false;
